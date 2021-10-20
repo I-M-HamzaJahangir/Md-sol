39e2b647044afd7126a193649e7c2ac413a9e80a
hamadhameed <hheview@gmail.com>
2021-10-20 12:01:50 +0500

    Worked on structure

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './assets/scss/mdsol-v1.scss';
+import "../node_modules/bootstrap/scss/bootstrap.scss";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
